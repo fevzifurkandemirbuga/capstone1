335b50d1106fedd6c957738a7e9d9203458f923f
fevzi <fevzifd2023@gmail.com>
2025-10-10 15:17:15 -0400

    ledger Screen added

---
+++ src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,45 @@
+package com.pluralsight;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Transaction {
+
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public Transaction(LocalDate date, LocalTime time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+
+    @Override
+    public String toString() {
+        return "-----------------------------------------" +
+                "\ndate= " + date +
+                "\ntime= " + time +
+                "\ndescription= '" + description + '\'' +
+                "\nvendor= '" + vendor + '\'' +
+                "\namount= " + amount +
+                "\n-----------------------------------------";
+    }
+}
--- src/main/java/com/pluralsight/TransactionApp.java
+++ src/main/java/com/pluralsight/TransactionApp.java
@@ -1,11 +1,19 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class TransactionApp {
     public static Scanner scan = new Scanner(System.in);
+    public static ArrayList<Transaction> transactions=new ArrayList<>();
     public static void main(String[] args) {
-
+        data();
         homeScreen();
         scan.close();
 
@@ -17,7 +25,7 @@ public class TransactionApp {
         boolean running=true;
         while(running){
             System.out.println("""
-                ***************  Home
+                ********  Home ********
                 D) Add Deposit
                 P) Make Payment(Debit)
                 L) Ledger
@@ -27,12 +35,92 @@ public class TransactionApp {
             switch (input){
                 case "D" -> {}
                 case "P" -> {}
-                case "L" -> {}
+                case "L" -> ledger();
                 case "X" -> running=false;
                 default -> System.out.println("wrong entry please try again.");
             }
         }
     }
+
+    public static void ledger(){
+
+        boolean running=true;
+        while(running){
+            System.out.println("""
+                *******  Ledger *******
+                A) All
+                D) Deposits
+                P) Payments
+                R) Reports
+                H) Home""");
+            System.out.print("your choose: ");
+            String input=scan.nextLine().toUpperCase();
+            switch (input){
+                case "A" -> {
+                    for(Transaction t:transactions){
+                        System.out.println(t);
+                    }
+                }
+                case "D" -> {
+                    for(Transaction t:transactions){
+                        if(t.getAmount()>0){
+                            System.out.println(t);
+                        }
+                    }
+                }
+                case "P" -> {
+                    for(Transaction t:transactions){
+                        if(t.getAmount()<0){
+                            System.out.println(t);
+                        }
+                    }
+                }
+                case "R" -> {}
+                case "H" -> running=false;
+                default -> System.out.println("wrong entry please try again.");
+            }
+        }
+
+    }
+
+    public static void data(){
+
+        try{
+            FileReader fr=new FileReader("transactions.csv");
+            BufferedReader bf=new BufferedReader(fr);
+            String input;
+            bf.readLine();
+            while((input=bf.readLine())!=null){
+                String[] data =input.split("\\|");
+
+//                convert String date to LocalDate
+                String[] stringDate=data[0].split("-");
+                LocalDate date= LocalDate.of(Integer.parseInt(stringDate[0]),
+                        Integer.parseInt(stringDate[1]),Integer.parseInt(stringDate[2]));
+
+//              convert String time to LocalTime
+                String[] stringTime=data[1].split(":");
+                LocalTime time=LocalTime.of(Integer.parseInt(stringTime[0]),
+                        Integer.parseInt(stringTime[1]),Integer.parseInt(stringTime[2]));
+
+//              convert string amount to double
+                double amount=Double.parseDouble(data[4]);
+
+                transactions.add(new Transaction(date,time,data[2],data[3],amount));
+
+            }
+            bf.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        transactions.sort(Comparator
+                .comparing(Transaction::getDate)
+                .thenComparing(Transaction::getTime));
+
+
+    }
+
 
 
 
